d1434e3212fc13afcaab7e6feaba7ca0dbea5a3f
Wahyudi-Satriawan-1B-TI <wahyudisatriawanh@gmail.com>
2025-02-21 09:20:00 +0700

    percobaan 3

--- Jobsheet2/MahasiswaMain.java
+++ Jobsheet2/MahasiswaMain.java
@@ -3,14 +3,18 @@ package Jobsheet2;
 public class MahasiswaMain {
     public static void main(String[] args) {
         Mahasiswa25 mhs1 = new Mahasiswa25();
-        mhs1.nama = "Wahyudi Satriawan Hamid";
+        mhs1.nama = "Muhammad Ali Farhan";
         mhs1.nim = "2241720171";
-        mhs1.kelas = "TI 1B";
-        mhs1.ipk = 3.7;
+        mhs1.kelas = "SI 2J";
+        mhs1.ipk = 3.55;
 
         mhs1.tampilkanInformasi();
-        mhs1.ubahKelas("TI 2B");
-        mhs1.updateIpk(4.0);
+        mhs1.ubahKelas("SI 2K");
+        mhs1.updateIpk(3.60);
         mhs1.tampilkanInformasi();
+
+        Mahasiswa25 mhs2 = new Mahasiswa25("Annisa Nabila", "2141720160",3.25, "TI 2L");
+        mhs2.updateIpk(3.30);
+        mhs2.tampilkanInformasi();
     }
 }
