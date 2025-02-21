eb13f3e1fab5586c287f87c5a8098dbb9d0c2729
Wahyudi-Satriawan-1B-TI <wahyudisatriawanh@gmail.com>
2025-02-21 10:55:41 +0700

    Perbaikan

--- Jobsheet2/MataKuliahMain25.java
+++ Jobsheet2/MataKuliahMain25.java
@@ -2,25 +2,25 @@ package Jobsheet2;
 
 public class MataKuliahMain25 {
     public static void main(String[] args) {
-        MataKuliah25 mhs1 = new MataKuliah25();
-        mhs1.kodeMK = "ALSD";
-        mhs1.nama = "Algoritma dan Struktur Data";
-        mhs1.sks = 3;
-        mhs1.jumlahJam = 6;
+        MataKuliah25 mk1 = new MataKuliah25();
+        mk1.kodeMK = "ALSD";
+        mk1.nama = "Algoritma dan Struktur Data";
+        mk1.sks = 3;
+        mk1.jumlahJam = 6;
 
-        mhs1.tampilkanInformasi();
-        mhs1.ubahSKS(2);
-        mhs1.kurangiJam(2);
+        mk1.tampilkanInformasi();
+        mk1.ubahSKS(2);
+        mk1.kurangiJam(2);
         System.out.println("==== Tampilan Informasi Yang Telah Diupdate ====");
         System.out.println("================================================");
-        mhs1.tampilkanInformasi();
+        mk1.tampilkanInformasi();
         
-        MataKuliah25 mhs2 = new MataKuliah25("PRAKALSD", "Praktikum Algoritma dan Struktur Data",2, 4);
-        mhs2.tampilkanInformasi();
-        mhs1.ubahSKS(3);
-        mhs1.tambahJam(2);
-        System.out.println("==== Tampilan Informasi Yang Telah Diupdate ====");
+        MataKuliah25 mk2 = new MataKuliah25("PRAKALSD", "Praktikum Algoritma dan Struktur Data",2, 4);
+        mk2.tampilkanInformasi();
+        mk2.ubahSKS(3);
+        mk2.tambahJam(2);
+        System.out.println("====mk2 Tampilan Informasi Yang Telah Diupdate ====");
         System.out.println("================================================");
-        mhs2.tampilkanInformasi();
+        mk2.tampilkanInformasi();
     }
 }
