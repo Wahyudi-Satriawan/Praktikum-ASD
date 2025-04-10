b7e9683989f57e33242686b3f815d3908a65565f
Wahyudi-Satriawan-1B-TI <wahyudisatriawanh@gmail.com>
2025-04-10 18:32:58 +0700

    save

---
+++ Jobsheet7/MahasiswaDemo25.java
@@ -0,0 +1,48 @@
+package Jobsheet7;
+
+import java.util.Scanner;
+
+public class MahasiswaDemo25 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int jumMhs=5;
+        
+        MahasiswaBerprestasi25 list = new MahasiswaBerprestasi25(jumMhs);
+
+        for (int i = 0; i < jumMhs; i++) {
+            System.out.println("Masukkan data mahasiswa ke-" + (i + 1));
+            System.out.print("NIM: ");
+            String nim = input.nextLine();
+            System.out.print("Nama: ");
+            String nama = input.nextLine();
+            System.out.print("Kelas: ");
+            String kelas = input.nextLine();
+            System.out.print("IPK: ");
+            double ipk = input.nextDouble();
+            input.nextLine();
+
+            Mahasiswa25 m = new Mahasiswa25(nim, nama, kelas, ipk);
+            list.tambah(m);
+        }
+
+        System.out.println("\nData mahasiswa sebelum sorting:");
+        list.tampil();
+
+        System.out.println("\nData mahasiswa setelah sorting berdasarkan IPK (DESC):");
+        list.bubbleSort();
+        list.tampil();
+        
+        System.out.println("\nData yang sudah terurut menggunakan SELECTION SORT (ASC):");
+        list.selectionSort();
+        list.tampil();
+        
+        System.out.println("\nData yang sudah terurut menggunakan INSERTION SORT (ASC):");
+        list.insertionSort();
+        list.tampil();
+
+
+
+        input.close();
+    }
+}
+
